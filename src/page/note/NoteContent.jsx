2f2054209bff6c339cc6f68ac478d8b1c8321d41
weibin.xu <xuwb@shanshan-business.com>
2025-02-12 18:25:49 +0800

    笔记保存

--- src/page/note/NoteContent.jsx
+++ src/page/note/NoteContent.jsx
@@ -1,14 +1,46 @@
 import './Note.css'
 import {useRef, useState} from "react";
-import Util from "../util/util.js";
 import constant from "./constant.js";
 import ajax from "../util/ajax.js";
+import key from "../util/key.js";
 
 function NoteContent({note}) {
 
   const [contentList, setContentList] = useState(() => {
     ajax.get('/note-content/list', {noteId: note.id}).then(res => {
-      setContentList(res.data || []);
+      let list = res.data;
+      if (!!list && list.length > 0) {
+        const tableIds = list.filter(a => a.type === constant.note_type_table).map(a => a.id)
+        ajax.get('/note-content/table-data-list', {'contentIds': tableIds}).then(res => {
+          if (res.code === 0 && !!res.data) {
+            const datasMap = res.data.reduce((a, b) => {
+              let tt = a[b.contentId]
+              if (!tt) tt = [];
+              if (!!b.datas) {
+                b.datas = JSON.parse(b.datas);
+              }
+              tt.push(b);
+              a[b.contentId] = tt;
+              return a;
+            }, {})
+            list.filter(a => a.type === constant.note_type_table)
+              .forEach(a => {
+                a.heads = JSON.parse(a.heads);
+                a.orders = JSON.parse(a.orders);
+                a.datas = datasMap[a.id]
+                a.datas.filter(cc => !cc.datas).forEach(cc => {
+                  cc.datas = []
+                  a.heads.forEach(_ => cc.datas.push(''))
+                })
+                a.datas = sortList(a.datas);
+              })
+          }
+          setContentList(list);
+        })
+      } else {
+        list = [];
+        setContentList(list);
+      }
     })
   });
   const inputDom = useRef(null);
@@ -43,14 +75,13 @@ function NoteContent({note}) {
         case '/table':
           content = {
             type: constant.note_type_table,
-            props: {
-              heads: [{
-                name: "field1"
-              }, {
-                name: "field2"
-              }]
-            },
-            datas: [{}, {}]
+            heads: ['field1', 'field2'],
+            orders: [0, 1],
+            datas: [
+              {
+                datas: ['', '']
+              }
+            ]
           }
           break;
       }
@@ -58,9 +89,19 @@ function NoteContent({note}) {
     if (!content) {
       content = {content: val, type: constant.note_type_text};
     }
-    ajax.post('/note-content/add', content).then(res => {
+    const addContent = {...content}
+    if (addContent.type === constant.note_type_table) {
+      addContent.heads = JSON.stringify(addContent.heads);
+      addContent.orders = JSON.stringify(addContent.orders);
+      addContent.datas.forEach(a => a.datas = JSON.stringify(a.datas))
+    }
+    ajax.post('/note-content/add', addContent).then(res => {
       if (res.code === 0) {
         content.id = res.data.id;
+        if (content.type === constant.note_type_table && res.data.datas?.length > 0) {
+          content.datas = res.data.datas;
+          content.datas.filter(a => !!a.datas).forEach(a => a.datas = JSON.parse(a.datas))
+        }
         setContentList([...contentList, content]);
         e.target.value = '';
       }
@@ -70,6 +111,7 @@ function NoteContent({note}) {
   function todoContentChange(e, item) {
     e.stopPropagation()
     const val = e.target.value
+    item.focus = false;
     if (val !== item.content) {
       ajax.post('/note-content/update', {id: item.id, content: val}).then(res => {
         if (res.code === 0) {
@@ -77,35 +119,65 @@ function NoteContent({note}) {
           setContentList([...contentList])
         }
       })
+    } else {
+      setContentList([...contentList])
     }
   }
 
   function enterToSaveTodoContent(e, contents, index, item) {
     e.stopPropagation()
-    if (e.keyCode === 13) {
+    key.onEnter(e, () => {
+      // enter
       if (item.content !== e.target.value) {
         item.content = e.target.value;
       }
       const newContent = initTodoContent([])
       newContent.afterId = item.id;
 
-      Promise.all([ajax.post('/note-content/update', item), ajax.post('/note-content/add', newContent)]).then(([res1, res2]) => {
+      const beforeContent = contents.filter(a => item.id === a.afterId)[0];
+
+      Promise.all([ajax.post('/note-content/update', item), ajax.post('/note-content/add', newContent)]).then(([_, res2]) => {
         if (res2.code === 0) {
           newContent.id = res2.data.id;
+          item.focus = false;
+          newContent.focus = true;
           contents.splice(index + 1, 0, newContent)
-        }
-        setContentList([...contentList])
-      });
-    } else if (e.keyCode === 8) {
-      if (!e.target.value) {
-        ajax.post('/note-content/update', {id: item.id, deleted: true}).then(res => {
-          if (res.code === 0) {
-            contents.splice(index, 1);
+          if (!!beforeContent) {
+            ajax.post('/note-content/update', {
+              id: beforeContent.id,
+              afterId: newContent.afterId
+            }).then(() => {
+              beforeContent.afterId = newContent.id;
+              setContentList([...contentList])
+            });
+          } else {
             setContentList([...contentList])
           }
-        })
+        }
+      });
+    })
+    key.onDelete(e, () => {
+      // delete
+      if (!e.target.value) {
+        if (!!item.isEmpty) {
+          // 如果已经是空了，则删掉
+          ajax.post('/note-content/update', {id: item.id, deleted: true}).then(res => {
+            if (res.code === 0) {
+              contents.splice(index, 1);
+              item.focus = false;
+              if (index > 0) {
+                contents[index - 1].focus = true;
+                document.getElementById(`content-item-todo-${contents[index - 1].id}`)?.focus();
+              }
+              setContentList([...contentList])
+            }
+          })
+        } else {
+          // 设置成空，下次再按退格，就删掉
+          item.isEmpty = true;
+        }
       }
-    }
+    })
   }
 
   function todoCheckChange(item) {
@@ -117,19 +189,102 @@ function NoteContent({note}) {
     })
   }
 
+  function headNameChange(e, item, index) {
+    e.stopPropagation()
+    const heads = [...item.heads]
+    heads[index] = e.target.value
+    ajax.post('/note-content/update', {id: item.id, heads: JSON.stringify(heads)}).then(res => {
+      if (res.code === 0) {
+        item.heads = [...heads]
+        setContentList([...contentList])
+      }
+    })
+  }
+
+  function tableDataChange(e, item, index) {
+    e.stopPropagation()
+    const datas = [...item.datas]
+    datas[index] = e.target.value
+    ajax.post('/note-content/table-data-update', {id: item.id, datas: JSON.stringify(datas)}).then(res => {
+      if (res.code === 0) {
+        item.datas = [...datas]
+        setContentList([...contentList])
+      }
+    })
+  }
+
+  function sortList(list){
+    if(!!list){
+      list.sort((a, b) => a.id - b.id);
+      const newContents = [], hasAfterIdContents = [];
+      list.forEach(a => {
+        if (!!a.afterId || !!a.beforeId) {
+          hasAfterIdContents.push(a);
+        } else {
+          newContents.push(a);
+        }
+      })
+      const newList = [];
+      hasAfterIdContents.forEach(a => {
+        let index = -1;
+        for (let i = 0; i < newContents.length; i++) {
+          if (newContents[i].id === a.beforeId) {
+            index = i;
+            break;
+          }
+        }
+        if (index === -1) {
+          newList.push(a);
+        } else {
+          newContents.splice(index, 0, a);
+        }
+      })
+      newList.forEach(a => {
+        let index = -1;
+        for (let i = 0; i < newContents.length; i++) {
+          if (newContents[i].id === a.afterId) {
+            index = i;
+            break;
+          }
+        }
+        if (index === -1) {
+          newContents.push(a);
+        } else {
+          newContents.splice(index + 1, 0, a);
+        }
+      })
+      list = [...newContents]
+    }
+    return list;
+  }
+
   function renderItemDom(contents) {
+    // 做一次排序
+    contents = sortList(contents);
+
+    let lastType = -1;
     return <>
       {
         !!contents && contents.map((a, index) => {
+          let hasMargin = '';
+          if (a.type === constant.note_type_todo) {
+            if (lastType !== a.type) {
+              hasMargin = 'has-margin';
+            }
+          } else {
+            hasMargin = 'has-margin';
+          }
+          lastType = a.type;
           return (
-            <div key={a.id} className={`note-content-list-item item-${a.type}`} onClick={e => e.stopPropagation()}>
+            <div key={a.id} className={`note-content-list-item item-${a.type} ${hasMargin}`}
+                 onClick={e => e.stopPropagation()}>
               {a.type === constant.note_type_page && <div>{a.content}</div>}
               {a.type === constant.note_type_text && <div>{a.content}</div>}
               {
                 a.type === constant.note_type_todo &&
                 <>
-                  <input type='checkbox' checked={!!a.checked} onChange={e => todoCheckChange(a)}/>
-                  <input type='text' defaultValue={a.content}
+                  <input type='checkbox' checked={!!a.checked} onChange={() => todoCheckChange(a)}/>
+                  <input id={`content-item-todo-${a.id}`} type='text' defaultValue={a.content} autoFocus={!!a.focus}
                          onBlur={e => todoContentChange(e, a)}
                          onKeyUp={e => enterToSaveTodoContent(e, contents, index, a)}/>
                 </>
@@ -140,20 +295,33 @@ function NoteContent({note}) {
                   <thead>
                   <tr>
                     {
-                      a.props.heads.map(head => {
-                        return <th>{head.name}</th>
+                      a.orders.filter(o => o > -1).map(o => {
+                        return <th key={`table-head-${o}`}>
+                          <div className='table-add-field'>+</div>
+                          <input type='text' defaultValue={a.heads[o]} onBlur={e => headNameChange(e, a, o)}/>
+                          <div className='table-add-field'>+</div>
+                        </th>
                       })
                     }
                   </tr>
                   </thead>
                   <tbody>
-                  <tr>
-                    {
-                      a.datas.map(data => {
-                        return <td>{data.value}</td>
-                      })
-                    }
-                  </tr>
+                  {
+                    a.datas.map(data => {
+                      return (
+                        <tr key={`item-table-data-${data.id}`}>
+                          {
+                            a.orders.filter(o => o > -1).map(o => {
+                              return <td key={`table-data-${data.id}-${o}`}>
+                                <input type='text' defaultValue={data.datas[o]}
+                                       onBlur={e => tableDataChange(e, data, o)}/>
+                              </td>
+                            })
+                          }
+                        </tr>
+                      )
+                    })
+                  }
                   </tbody>
                 </table>
               }
---
+++ src/page/util/key.js
@@ -0,0 +1,42 @@
+/**
+ * 按键监听
+ * @param e
+ * @param keyCode
+ * @param callback
+ * @param ctrl 是否按住功能键 'shift' | 'ctrl' | 'alt'
+ */
+function onKeyMatch(e, keyCode, callback, ctrl) {
+  if (!callback) return;
+
+  if (keyCode === e.keyCode) {
+    if (!!ctrl) {
+      let controller = false;
+      switch (ctrl) {
+        case 'ctrl':
+          controller = e.ctrlKey;
+          break;
+        case 'alt':
+          controller = e.altKey;
+          break;
+        case 'shift':
+          controller = e.shiftKey;
+          break;
+      }
+      if (!controller) return;
+    }
+    callback();
+  }
+}
+
+function onEnter(e, callback, ctrl) {
+  onKeyMatch(e, 13, callback, ctrl)
+}
+
+function onDelete(e, callback, ctrl) {
+  onKeyMatch(e, 8, callback, ctrl)
+}
+
+export default {
+  onEnter,
+  onDelete
+}
